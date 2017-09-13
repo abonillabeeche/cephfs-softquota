e708f02a86f992435bb454395b798fc7b21eb1ec
Alejandro Bonilla <abonilla@suse.com>
2017-09-13 09:03:24 -0400

    initial commit
    	new file:   README.md
    	new file:   usage.sh
    	new file:   usermap

---
+++ usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# source our user Array
+
+source /root/usermap
+
+#For each key in the usermap, check the quota value and send an e-mail if exceeded.
+
+for homedir in ${!USERMAP[@]};
+do
+
+if [ $( getfattr -d -m ceph.dir.rbytes /mnt/$homedir | grep -o '[0-9]*' ) -gt ${USERMAP[$homedir]} ]; 
+then echo | mail -s "Home Directory for user $homedir exceeded quota" abonilla@suse.com
+sleep 1
+fi
+done
